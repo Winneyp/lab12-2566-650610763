c6b1a93239a3b57219aa8bb3ff4f05d6b5c8b6e1
P <tatakornaow@gmail.com>
2023-08-03 15:41:42 +0700

    done

--- src/app/page.js
+++ src/app/page.js
@@ -78,7 +78,12 @@ export default function HomePage() {
           <Controller />
         </LangContext.Provider>
       </ThemeContext.Provider>
-      <Footer year="2023" fullName="Chayanin Suatap" studentId="650610560" />
+      <Footer
+        year="2023"
+        fullName="Tayakorn Aowrattanakul"
+        studentId="650610763"
+      />
     </div>
   );
+  1;
 }
